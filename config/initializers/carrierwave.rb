c8d2c3542d7634fb9ca1e366dbbc2f77d36e4712
Nick Taylor <nick@pouncingfox.com>
2018-05-10 11:14:37 -0400

    Region change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,11 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
-    :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
+    :s3_access_key_id      => ENV['AKIAIOTMEAAQNWP4IJ5Q'],                        # required
+    :s3_secret_access_key  => ENV['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'],                     # required
+    :region                => 'us-west-1'
   }
-  config.fog_directory  = ENV['S3_Bucket']                             # required
+  config.fog_directory  = ENV['rails-tutorial-pouncingfox']                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
