2b77233c81e6b22e1a5235df08ea7a66182847db
Nick Taylor <nick@pouncingfox.com>
2018-05-10 12:12:37 -0400

    Please work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
-
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :s3_access_key_id      => ENV['AKIAIOTMEAAQNWP4IJ5Q'],                        # required
-    :s3_secret_access_key  => ENV['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'],                     # required
-    :region                 => 'us-west-1',                  # optional, defaults to 'us-east-1'
-  }
-  #config.fog_directory  = ENV['rails-tutorial-pouncingfox']                             # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-end
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
